d21d52fda10f3db58629a086796506d405a0d1e8
jonathanhchiu <jonathanchiu95@gmail.com>
2016-03-07 22:49:27 -0800

    fixed add

--- AddEvent.java
+++ AddEvent.java
@@ -2,6 +2,7 @@ package calendar;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -25,9 +26,15 @@ public class AddEvent implements EventHandler<ActionEvent> {
     protected TextField yearEnd;
     protected TextField timeEnd;
     protected TextField titleText;
-    protected ComboBox AMPMStart;
-    protected ComboBox AMPMEnd;
+    protected TextField AMPMStart;
+    protected TextField AMPMEnd;
     
+    
+    
+    /**
+     * Displays the GridPane with the new event form.
+     * @param e 
+     */
     @Override
     public void handle(ActionEvent e) {
         //grab currently selected date
@@ -49,14 +56,14 @@ public class AddEvent implements EventHandler<ActionEvent> {
         Label title = new Label("Title: ");
         Label start = new Label("Starts: ");
         Label end = new Label("Ends: ");
-        Button submit = new Button("Submit");
         GridPane eventGrid = new GridPane();
-        AMPMStart = new ComboBox();
-        AMPMEnd = new ComboBox();
+        AMPMStart = new TextField();
+        AMPMEnd = new TextField();
         
-        //add options for combobox
-        AMPMStart.getItems().addAll("AM", "PM");
-        AMPMEnd.getItems().addAll("AM", "PM");
+        //set event listener to button
+        Button submit = new Button("Submit");
+        HandleEvent processEvent = new HandleEvent();
+        submit.setOnAction(processEvent);
         
         //set prompt text
         titleText.setPromptText("Untitled Event");
@@ -68,6 +75,8 @@ public class AddEvent implements EventHandler<ActionEvent> {
         yearEnd.setPromptText("2016");
         timeStart.setPromptText("8:00");
         timeEnd.setPromptText("9:00");
+        AMPMStart.setPromptText("AM");
+        AMPMEnd.setPromptText("PM");
         
         //add everything to GridPane
         eventGrid.add(addNew, 1, 0, 1, 3);
@@ -91,10 +100,124 @@ public class AddEvent implements EventHandler<ActionEvent> {
         
         //replace old GridPane with this one
         Calendar.app.setCenter(eventGrid);
+    }
+    
+    
+    
+    /**
+     * Inner class that listens to the "Add Event" button
+     */
+    public class HandleEvent implements EventHandler<ActionEvent> {
+        protected String title;
+        protected String monthBegin;
+        protected String dayBegin;
+        protected String yearBegin;
+        protected String timeBegin;
+        protected String AMPMBegin;
+        protected String monthFinish;
+        protected String dayFinish;
+        protected String yearFinish;
+        protected String timeFinish;
+        protected String AMPMFinish;
         
-        //add inner class
-        //check if file exists with corresponding month
-            //if not, make one and write
-            //if so, filestream open it and append it
+        
+        
+        /**
+         * Checks and saves the data in the new event form.
+         * @param e 
+         */
+        @Override
+        public void handle(ActionEvent e) {
+            
+            //perform checks to ensure all fields are filled out
+            title = titleText.getText();
+            monthBegin = monthStart.getText();
+            dayBegin = dayStart.getText();
+            yearBegin = yearStart.getText();
+            timeBegin = timeStart.getText();
+            AMPMBegin = AMPMStart.getText();
+            
+            monthFinish = monthEnd.getText();
+            dayFinish = dayEnd.getText();
+            yearFinish = yearEnd.getText();
+            timeFinish = timeEnd.getText();
+            AMPMFinish = AMPMEnd.getText();
+            Alert errorAlert;
+            
+            //no title
+            if (title.equals("")) {
+                //alert
+                errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setTitle("Error");
+                errorAlert.setHeaderText("No event title specified: ");
+                errorAlert.setContentText("Please name your event.");
+            
+                errorAlert.showAndWait();
+                return;
+            }
+            //no month
+            if (monthBegin.equals("") || monthFinish.equals("")) {
+                //alert
+                errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setTitle("Error");
+                errorAlert.setHeaderText("No event month specified: ");
+                errorAlert.setContentText("Please specify a month.");
+            
+                errorAlert.showAndWait();
+                return;
+            }
+            //no day
+            if (dayBegin.equals("") || dayFinish.equals("")) {
+                //alert
+                errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setTitle("Error");
+                errorAlert.setHeaderText("No event day specified: ");
+                errorAlert.setContentText("Please specify a day.");
+            
+                errorAlert.showAndWait();
+                return;
+            }
+            //no year
+            if (yearBegin.equals("") || yearFinish.equals("")) {
+                //alert
+                errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setTitle("Error");
+                errorAlert.setHeaderText("No event year specified: ");
+                errorAlert.setContentText("Please specify a year.");
+            
+                errorAlert.showAndWait();
+                return;
+            }
+            //no time
+            if (timeBegin.equals("") || timeBegin.equals("")) {
+                //alert
+                errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setTitle("Error");
+                errorAlert.setHeaderText("No event time specified: ");
+                errorAlert.setContentText("Please specify a time.");
+            
+                errorAlert.showAndWait();
+                return;
+            }
+            //no AM/PM
+            if (AMPMBegin.equals("") || AMPMFinish.equals("")) {
+                //alert
+                errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setTitle("Error");
+                errorAlert.setHeaderText("AM/PM not specified: ");
+                errorAlert.setContentText("Please specify whether the event is in the morning or night");
+            
+                errorAlert.showAndWait();
+                return;
+            }
+            
+            
+            //return to calendar view
+            int month = Integer.parseInt(Calendar.monthNum.getText());
+            int year = Integer.parseInt(Calendar.year.getText());
+            SetupGUI backToCal = new SetupGUI(month, year);
+            Calendar.app.setCenter(backToCal.calendarSpace);
+ 
+        }
     }
 }
